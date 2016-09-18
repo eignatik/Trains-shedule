762a29441c7304b2d9eaefacd9ca4bf80aad3630
eignatik <light.ji@yandex.ru>
2016-09-18 14:11:07 -0400

    Added listener for button to launch scheule activity

--- app/src/main/java/com/train_shedule/trains_shedule/MainActivity.java
+++ app/src/main/java/com/train_shedule/trains_shedule/MainActivity.java
@@ -1,13 +1,36 @@
 package com.train_shedule.trains_shedule;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.btnStart: {
+                startFirstPageActivity();
+                break;
+            }
+        }
+    }
+
+    private void startFirstPageActivity(){
+        Intent intent = new Intent(this, ScheduleMain.class);
+        startActivity(intent);
     }
 }
